1cb727914b1d255589ccae2ff92e5e33751cda12
DanielAbbasi21 <daniel-2003@hotmail.se>
2026-04-19 19:05:43 +0200

    feat: connect to MongoDB Atlas using environment variables

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -6,8 +8,17 @@ app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
+mongoose.connect(process.env.CONNECTION_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((err) => console.log(err));
