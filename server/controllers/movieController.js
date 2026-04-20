99897cf66d9162d696454a70cbebe6112f92a850
DanielAbbasi21 <daniel-2003@hotmail.se>
2026-04-20 13:09:14 +0200

    feat: add DELETE route for movies

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,4 +1,5 @@
 const Movie = require("../models/Movie");
+const mongoose = require("mongoose");
 
 // POST - create movie
 const createMovie = async (req, res) => {
@@ -32,7 +33,7 @@ const getMovies = async (req, res) => {
   }
 };
 
-//PUT - update movide
+//PUT - update movie
 const updateMovie = async (req, res) => {
   try {
     const updatedMovie = await Movie.findByIdAndUpdate(
@@ -51,8 +52,29 @@ const updateMovie = async (req, res) => {
   }
 };
 
+//DELETE - delete movie
+const deleteMovie = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ error: "Invalid movie ID" });
+    }
+
+    const deletedMovie = await Movie.findByIdAndDelete(req.params.id);
+
+    if (!deletedMovie) {
+      return res.status(404).json({ error: "Movie not found" });
+    }
+
+    res.status(200).json({ message: "Movie deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createMovie,
   getMovies,
-  updateMovie
+  updateMovie,
+  deleteMovie
 };
