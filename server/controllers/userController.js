8528506c6832da3cc48b03d1fd97af9275d13585
DanielAbbasi21 <daniel-2003@hotmail.se>
2026-04-21 11:01:58 +0200

    feat: add DELETE route for users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -77,4 +77,24 @@ const updateUser = async (req, res) => {
 };
 
 
-module.exports = { createUser, getUsers, getUserById, updateUser };
+const deleteUser = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ error: "Invalid user ID" });
+    }
+
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+module.exports = { createUser, getUsers, getUserById, updateUser, deleteUser };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser, getUsers, getUserById, updateUser } = require("../controllers/userController");
+const { createUser, getUsers, getUserById, updateUser, deleteUser } = require("../controllers/userController");
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
