53d094ff117fa89d8e3a97cf8819c14b9f1c7470
DanielAbbasi21 <daniel-2003@hotmail.se>
2026-04-22 11:36:44 +0200

    feat: add Review model with relations

---
+++ server/models/Review.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Movie",
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 10
+  },
+  comment: {
+    type: String
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Review", reviewSchema);
