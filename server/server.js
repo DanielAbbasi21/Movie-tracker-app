4987e355b4eea294308a7feaf229cfc82491631b
DanielAbbasi21 <daniel-2003@hotmail.se>
2026-05-05 15:02:19 +0200

    fix: configure environment loading for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config({ path: "./server/.env" });
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
